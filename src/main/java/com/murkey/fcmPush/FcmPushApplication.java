255c2bb0253b0fd53ef39f210faa00e6c776adb2
DESKTOP-OIVS5LI\USER <shs1494@naver.com>
2023-03-07 02:28:18 +0900

    Spring Boot 생성

---
+++ src/main/java/com/murkey/fcmPush/FcmPushApplication.java
@@ -0,0 +1,13 @@
+package com.murkey.fcmPush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcmPushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcmPushApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/murkey/fcmPush/FcmPushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murkey.fcmPush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcmPushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
